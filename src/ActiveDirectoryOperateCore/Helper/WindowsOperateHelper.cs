18652f98743162bfcdbf3a5ee0c55ad463baf1c1
cdcd72 <cdcd71517@gmail.com>
2020-06-25 13:22:39 +0800

    Initial project

---
+++ src/ActiveDirectoryOperateCore/Helper/WindowsOperateHelper.cs
@@ -0,0 +1,42 @@
+﻿using ActiveDirectoryOperateCore.Enum;
+using System;
+using System.Collections.Generic;
+using System.DirectoryServices;
+
+namespace ActiveDirectoryOperateCore.Helper
+{
+    public static class WindowsOperateHelper
+    {
+        // Account disable flag
+        private const int ACCOUNTDISABLE_FLAG = (int)AdsUserFlag.ADS_UF_ACCOUNTDISABLE;
+
+        /// <summary>
+        /// Get actived windows user directory entries
+        /// </summary>
+        /// <returns></returns>
+        public static List<DirectoryEntry> GetWindowsActiveUserDirectoryEntries()
+        {
+            List<DirectoryEntry> directoryEntries = new List<DirectoryEntry>();
+
+            // Get local machine directory entry
+            DirectoryEntry localMachineDirectoryEntry = new DirectoryEntry("WinNT://" + Environment.MachineName);
+
+            foreach (DirectoryEntry childDirectoryEntry in localMachineDirectoryEntry.Children)
+            {
+                // Focus user directory entry
+                if (childDirectoryEntry.SchemaClassName == "User")
+                {
+                    var userflags = (int)childDirectoryEntry.Properties["UserFlags"].Value;
+
+                    // If userflags bitwise and accountDisableFlag equal two, mean this windows user state is inactive
+                    if ((userflags & ACCOUNTDISABLE_FLAG) != ACCOUNTDISABLE_FLAG)
+                    {
+                        directoryEntries.Add(childDirectoryEntry);
+                    }
+                }
+            }
+
+            return directoryEntries;
+        }
+    }
+}
---
+++ src/ConsoleApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using ActiveDirectoryOperateCore.Helper;
+using System;
+using System.Linq;
+
+namespace ConsoleApp
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length > 0)
+            {
+                var methodName = args[0];
+
+                switch (methodName)
+                {
+                    case "DisplayWindowsActiveUserNames":
+                        DisplayWindowsActiveUserNames();
+                        break;
+                    default:
+                        break;
+                }
+            } 
+            else
+            {
+                DisplayWindowsActiveUserNames();
+            }
+        }
+
+        /// <summary>
+        /// Display actived windows user names
+        /// </summary>
+        private static void DisplayWindowsActiveUserNames()
+        {
+            // Get actived windows user names
+            var windowsActiveUserNames =
+                WindowsOperateHelper.GetWindowsActiveUserDirectoryEntries()
+                                    .Select(windowsActiveUserDirectoryEntry => windowsActiveUserDirectoryEntry.Name);
+
+            foreach (var windowsActiveUserName in windowsActiveUserNames)
+            {
+                Console.WriteLine(windowsActiveUserName);
+            }
+        }
+    }
+}
